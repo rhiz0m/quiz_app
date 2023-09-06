391191b6d8d31efa78be4ef02aa59714c0ab26b3
taokyone <antonsson.andreas@hotmail.com>
2023-09-06 14:40:24 +0200

    simple design for quiz app

--- quizapp/ViewController.swift
+++ quizapp/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionTextFeild: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
